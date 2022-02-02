808dd283f8ca5f91a364802b26f99eafb13285d2
Luis Escobedo <lescobedo.im@gmail.com>
2022-02-02 07:08:14 -0500

    Api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Gather data from API"""
+
+from requests import get
+from sys import argv, exit
+
+if __name__ == "__main__":
+    try:
+        id = arv[1]
+        is_integer = int(id)
+    except Exception:
+        exit()
+
+    url = "https://jsonplaceholder.typicode.com/"
+    url_user = url + "users?id=" + id
+    url_todo = url + "todos?userId=" + id
+
+    request_user = get(url_user)
+    request_todo = get(url_todo)
+    try:
+        jsuser = request_user.json()
+        jstodo = request_todo.json()
+    except ValueError:
+        print("No Json")
+
+    if jsuser and jstodo:
+        EMPLOYEE_NAME = jsuser[0].get('name')
+        NUMBER_OF_DONE_TASKS = 0
+        for task in jstodo:
+            if task.get('completed'):
+                NUMBER_OF_DONE_TASKS += 1
+        TOTAL_NUMBER_OF_TASKS = len(jstodo)
+
+    # Print first line
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME,
+                  NUMBER_OF_DONE_TASKS,
+                  TOTAL_NUMBER_OF_TASKS))
+
+    # Second and N lines
+    for doing in jstodo:
+        TASK_TITLE = doing.get('title')
+        if doing.get('completed'):
+            int("\t {}".format(TASK_TITLE))
+
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Gather data from API"""
+
+from requests import get
+from sys import argv, exit
+import csv
+
+if __name__ == "__main__":
+    try:
+        id = arv[1]
+        is_integer = int(id)
+    except Exception:
+        exit()
+
+    url = "https://jsonplaceholder.typicode.com/"
+    url_user = url + "users?id=" + id
+    url_todo = url + "todos?userId=" + id
+
+    request_user = get(url_user)
+    request_todo = get(url_todo)
+    try:
+        jsuser = request_user.json()
+        jstodo = request_todo.json()
+    except ValueError:
+        print("No Json")
+
+    if jsuser and jstodo:
+        USER_ID = id
+        USERNAME = jsuser[0].get('username')
+
+        # export data in the CSV format
+        with open(id + '.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',',
+                                   quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in jstodo:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            csvwriter.writerow([USER_ID,
+                                USERNAME,
+                                TASK_COMPLETED_STATUS,
+                                TASK_TITLE])
+
