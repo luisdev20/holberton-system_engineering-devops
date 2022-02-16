ecfe70264b4550d5b4eee14be6affbbf0dd32249
luisdev20 <lescobedo.im@gmail.com>
2022-02-16 00:02:04 -0500

    advanced API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -41,7 +41,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         if len(instances) == 0:
             print("")
             return
-        instances = sorted(instances.items(), reverse=True,
+        instances = sorted(instances.items(),
                            key=lambda kv: (-kv[1], kv[0]))
         [print("{}: {}".format(k, v)) for k, v in instances]
     else:
